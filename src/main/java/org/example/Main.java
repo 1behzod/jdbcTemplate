53dc8095b4ffaa6d3e72cb49daba41c6a7a7af01
Behzod <behzodjaloliddinov870@gmail.com>
2024-12-16 10:33:40 +0500

    changes in Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 import org.example.config.PostgresConfig;
 import org.example.dao.UserDAO;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.jdbc.core.JdbcTemplate;
+
 
 public class Main {
 
@@ -16,7 +16,7 @@ public class Main {
         userDAO.insert("John Doe", 30);
         userDAO.getAllUsers().forEach(user ->
                 System.out.println(user.getId() + " " + user.getName() + " " + user.getAge()));
-        userDAO.update(1, "John Smith", 35);
+        userDAO.update(1, "Behzod Jaloliddinov", 18);
         userDAO.delete(1);
         context.close();
     }
